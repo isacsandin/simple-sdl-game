c1690f8cad41729131e5d485dba3206a2810336d
guixlherme@gmail.com <guixlherme@gmail.com@db0d8494-3886-ce6b-6df6-89dcaa28d0d2>
2013-02-01 16:46:04 +0000

    block e block check point

---
+++ SDLGame/src/CheckPointBlock.cpp
@@ -0,0 +1,22 @@
+/*
+ * CheckPointBlock.cpp
+ *
+ *  Created on: 01/02/2013
+ *      Author: guineri
+ */
+
+#include "CheckPointBlock.h"
+
+CheckPointBlock::CheckPointBlock() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CheckPointBlock::~CheckPointBlock() {
+	// TODO Auto-generated destructor stub
+}
+
+bool CheckPointBlock::CheckCollision(){
+	return false;
+}
+
