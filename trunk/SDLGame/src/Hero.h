a47e730699f4cb2c7f4e893dd0ef7d0fc32733e3
guixlherme@gmail.com <guixlherme@gmail.com@db0d8494-3886-ce6b-6df6-89dcaa28d0d2>
2013-02-01 16:31:19 +0000

    hero, enemy

--- trunk/SDLGame/src/Hero.h
+++ trunk/SDLGame/src/Hero.h
@@ -12,30 +12,11 @@
 #include "VisibleObject.h"
 
 class Hero: public MovableObject, public VisibleObject {
-private:
-	int life;
-	int damage;
 
 public:
 	Hero(int life, int damage);
 	virtual ~Hero();
 	void HandleInput();
-
-	int getDamage() const {
-		return damage;
-	}
-
-	void setDamage(int damage) {
-		this->damage = damage;
-	}
-
-	int getLife() const {
-		return life;
-	}
-
-	void setLife(int life) {
-		this->life = life;
-	}
 };
 
 #endif /* HERO_H_ */
