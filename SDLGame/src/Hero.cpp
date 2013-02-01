1feade3c2fa55c49fcc208b9afb1c4e3c852c54b
guixlherme@gmail.com <guixlherme@gmail.com@db0d8494-3886-ce6b-6df6-89dcaa28d0d2>
2013-02-01 16:23:23 +0000

    visibleObject, heroObject

---
+++ SDLGame/src/Hero.cpp
@@ -0,0 +1,22 @@
+/*
+ * Hero.cpp
+ *
+ *  Created on: 01/02/2013
+ *      Author: guineri
+ */
+
+#include "Hero.h"
+
+Hero::Hero(int life, int damage) {
+	// TODO Auto-generated constructor stub
+	setLife(life);
+	setDamage(damage);
+}
+
+Hero::~Hero() {
+	// TODO Auto-generated destructor stub
+}
+
+void Hero::HandleInput(){
+
+}
---
+++ SDLGame/src/VisibleObject.h
@@ -0,0 +1,18 @@
+/*
+ * VisibleObject.h
+ *
+ *  Created on: 01/02/2013
+ *      Author: guineri
+ */
+
+#ifndef VISIBLEOBJECT_H_
+#define VISIBLEOBJECT_H_
+
+class VisibleObject {
+public:
+	VisibleObject();
+	virtual ~VisibleObject();
+	bool show();
+};
+
+#endif /* VISIBLEOBJECT_H_ */
