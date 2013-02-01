7c541c03a386735ff125b9d34e251e0a328a7801
guixlherme@gmail.com <guixlherme@gmail.com@db0d8494-3886-ce6b-6df6-89dcaa28d0d2>
2013-02-01 16:12:25 +0000

    StaticObject

---
+++ trunk/SDLGame/src/StaticObject.h
@@ -0,0 +1,19 @@
+/*
+ * StaticObject.h
+ *
+ *  Created on: 01/02/2013
+ *      Author: guineri
+ */
+
+#ifndef STATICOBJECT_H_
+#define STATICOBJECT_H_
+
+#include "PhisicalObject.h"
+
+class StaticObject: public PhisicalObject {
+public:
+	StaticObject();
+	virtual ~StaticObject();
+};
+
+#endif /* STATICOBJECT_H_ */
