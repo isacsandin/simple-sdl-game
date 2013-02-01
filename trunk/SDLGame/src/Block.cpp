e1d5d21d4263f8e46c8ee7cbd915f3e93f17fbeb
guixlherme@gmail.com <guixlherme@gmail.com@db0d8494-3886-ce6b-6df6-89dcaa28d0d2>
2013-02-01 16:46:04 +0000

    block e block check point

---
+++ trunk/SDLGame/src/Block.cpp
@@ -0,0 +1,18 @@
+/*
+ * Block.cpp
+ *
+ *  Created on: 01/02/2013
+ *      Author: guineri
+ */
+
+#include "Block.h"
+
+Block::Block() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Block::~Block() {
+	// TODO Auto-generated destructor stub
+}
+
