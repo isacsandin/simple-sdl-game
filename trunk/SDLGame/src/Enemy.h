11eeab817b6cad02608ef3b734b59327ba242ba2
feliperviegas <feliperviegas@db0d8494-3886-ce6b-6df6-89dcaa28d0d2>
2013-02-02 18:31:38 +0000

    Inimigo Koopa!

---
+++ trunk/SDLGame/src/Enemy.h
@@ -0,0 +1,31 @@
+/*
+ * Enemy.h
+ *
+ *  Created on: 02/02/2013
+ *      Author: feliperviegas
+ */
+
+#ifndef ENEMY_H_
+#define ENEMY_H_
+#include "Utils.h"
+#include <vector>
+#include <SDL/SDL.h>
+#include "Utils.h"
+
+class Enemy {
+
+private:
+	SDL_Surface *koopa_l, *koopa_r;
+	SDL_Rect clip[3];
+	float x, y;
+	float xVel;
+	int moving;
+	int spriteFrame;
+public:
+	Enemy();
+	virtual ~Enemy();
+	void move(Uint32 deltaTicks);
+	void show(SDL_Surface *screen);
+};
+
+#endif /* ENEMY_H_ */
